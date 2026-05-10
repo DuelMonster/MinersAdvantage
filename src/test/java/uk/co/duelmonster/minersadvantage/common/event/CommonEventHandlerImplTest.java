b0481cc98dc8922eaed5d5e78df5058d8a907e50
DuelMonster <GitHub@DuelMonster.co.uk>
2026-05-10 21:43:47 +0100

    ✅test: add common event handler routing coverage

---
+++ src/test/java/uk/co/duelmonster/minersadvantage/common/event/CommonEventHandlerImplTest.java
@@ -0,0 +1,39 @@
+package uk.co.duelmonster.minersadvantage.common.event;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import uk.co.duelmonster.minersadvantage.common.MinersAdvantageCore;
+import uk.co.duelmonster.minersadvantage.common.feature.FeatureId;
+import uk.co.duelmonster.minersadvantage.common.orchestration.FeatureDispatchContext;
+
+class CommonEventHandlerImplTest {
+    @AfterEach
+    void clearObserver() {
+        FeatureEventHandler.resetDispatchObserverForTesting();
+    }
+
+    @Test
+    void routesPickaxeStoneToShaftanation() {
+        CommonEventHandlerImpl handler = new CommonEventHandlerImpl(new MinersAdvantageCore());
+        AtomicReference<FeatureDispatchContext> captured = new AtomicReference<>();
+        FeatureEventHandler.setDispatchObserverForTesting(captured::set);
+
+        handler.onPickaxeUse(1, 2, 3, "minecraft:stone");
+
+        assertEquals(FeatureId.SHAFTANATION, captured.get().feature());
+    }
+
+    @Test
+    void routesHoeCropToCropination() {
+        CommonEventHandlerImpl handler = new CommonEventHandlerImpl(new MinersAdvantageCore());
+        AtomicReference<FeatureDispatchContext> captured = new AtomicReference<>();
+        FeatureEventHandler.setDispatchObserverForTesting(captured::set);
+
+        handler.onHoeUse(1, 2, 3, "minecraft:wheat_crop");
+
+        assertEquals(FeatureId.CROPINATION, captured.get().feature());
+    }
+}
